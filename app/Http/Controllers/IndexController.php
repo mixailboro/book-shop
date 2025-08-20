e40b56a69b19c54a26ef3f0a41cdc17e549a0651
mikhailboro <M.Borodkin91@yandex.ru>
2025-08-20 21:50:14 +0300

    продолжили верстку

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use App\Models\Book;
+use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
@@ -12,7 +14,16 @@ class IndexController extends Controller
     {
         $books = Book::existBooks()->get();
         $authors = Author::whoseBooksExist()->get();
+        $cart = $this->getCart();
 
-        return view('index', compact('books', 'authors'));
+        return view('index', compact('books', 'authors', 'cart'));
+    }
+
+    private function getCart(): ?Cart
+    {
+        if (Auth::user() && Auth::user()->cart && !Auth::user()->cart->isEmpty()) {
+            return  Auth::user()->cart;
+        }
+        return null;
     }
 }
